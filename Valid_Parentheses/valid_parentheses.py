adc94f20be3ec1ce78a6921295b7ddd1d84d3549
pgarg <pl201920@dal.ca>
2021-06-18 08:25:30 -0300

    Third problem added

---
+++ Valid_Parentheses/valid_parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        pairs = { '(': ')' , '{': '}' , '[': ']'}
+        
+        stack = []
+        for elem in s:
+            if elem in pairs.keys():
+                stack.append(elem)
+            else:
+                if (len(stack) != 0):
+                    top = stack[len(stack) - 1]
+                else:
+                    top = 0
+                stack.append(elem)
+                if (pairs.get(top) == elem):
+                    stack.pop()
+                    stack.pop()
+              
+        if len(stack) == 0:
+            return True
+        
+        return False
