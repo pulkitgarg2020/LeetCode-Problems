a982eef0288e92c8327115c86f04a46ecf9283d7
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2021-09-09 11:21:57 -0300

    Create 8_valid_anagram.java

---
+++ 8_valid_anagram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if (s.length() != t.length()) return false;
+        else {
+            char[] a1 = s.toCharArray();
+            char[] a2 = t.toCharArray();
+            
+            Arrays.sort(a1);
+            Arrays.sort(a2);
+            
+            return Arrays.equals(a1, a2);            
+        }
+    }
+}
